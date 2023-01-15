ad2fc7eafead1a84646728a5c79e62528281e5c0
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2023-01-15 14:13:58 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+     Val int
+     Next *ListNode
+ }
+ 
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    return new(ListNode)
+}
+
+func main() {
+
+}
